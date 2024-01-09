56be212d0599a8448ae09e0e0f1a3afbac0672db
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-01-09 14:58:00 -0600

    revise the return-false-condition check

--- simple-problems/lc0009/Solution.java
+++ simple-problems/lc0009/Solution.java
@@ -7,15 +7,17 @@
  */
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x == 0)
-            return true;
-
-        if (x < 0 || x % 10 == 0)
+        // negative numbers are obviously not palindrome
+        if (x < 0)
             return false;
 
         int rev = 0;
         while (rev < x) {
             rev = (rev * 10) + (x % 10);
+            // all positive numbers with ending zeros are not palindrome
+            if (rev == 0) {
+                return false;
+            }
             x /= 10;
         }
         return (x == rev) || (x == rev / 10);
