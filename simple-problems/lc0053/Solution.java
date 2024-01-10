9b1c7a2462dfd57b3022a7f842859c505bc94507
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-01-10 14:07:47 -0600

    add LC.53 solution

---
+++ simple-problems/lc0053/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * @author: orc-dev
+ * @update: Jan.10 2024
+ * 
+ * @leetcode: 53. Maximum Subarray
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int acc = 0;
+        for (int x : nums) {
+            acc += x;
+            max = Math.max(max, acc);
+            // if partial sum 'acc' is non-positive, reset it to 0
+            acc = Math.max(acc, 0);  
+        }
+       return max; 
+    }
+}
