210813edc5fbddf484892d9e978162a226dc1b34
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-01-10 11:22:00 -0600

    add LC.42 solution

---
+++ simple-problems/lc0042/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * @author: orc-dev
+ * @update: Jan.10 2024
+ * 
+ * Log
+ *   - double pointers
+ *   - traversal from end to center
+ *   - move pointer from shorter-wall-side to taller-wall-side
+ * 
+ * Performance
+ *   - Runtime: ()
+ *   - Memory: ()
+ */
+class Solution {
+    public int trap(int[] ht) {
+        int water = 0;
+        int l = 0;              // left pointer
+        int r = ht.length - 1;  // right pointer
+        int L = ht[l];          // tallest wall on LSH
+        int R = ht[r];          // tallest wall on RSH
+
+        while (l + 1 < r) {
+            if (L <= R) {
+                l++;
+                L = Math.max(L, ht[l]);
+                water += Math.max(0, L - ht[l]);
+            } 
+            else {
+                r--;
+                R = Math.max(R, ht[r]);
+                water += Math.max(0, R - ht[r]);
+            }
+        }
+        return water;
+    }
+}
