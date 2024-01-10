d261e2fe6990d6244ca62c7e7d7b9f71d852c493
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-01-10 15:21:53 -0600

    revise dp.init

--- simple-problems/lc0062/Solution.java
+++ simple-problems/lc0062/Solution.java
@@ -4,9 +4,10 @@
  * 
  * @leetcode: 62.Unique Paths
  * 
- * DP tabular representation
+ * DP tabular representation (moore dp)
  *        0  1  2  3  4  5  6
- *     0 [1][1][1][1][1][1][1]
+ *       [1][ ][ ][ ][ ][ ][ ] <-- dp.init
+ *     0 [ ][ ][ ][ ][ ][ ][ ]
  *     1 [ ][ ][ ][ ][ ][ ][x]
  *     2 [ ][ ][ ][ ][ ][y][z]
  * 
@@ -15,9 +16,9 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         final int[] dp = new int[n];
-        java.util.Arrays.fill(dp, 1);
+        dp[0] = 1;
         
-        for (int r = 1; r < m; ++r) {
+        for (int r = 0; r < m; ++r) {
             for (int c = 1; c < n; ++c) {
                 dp[c] += dp[c - 1];
             }
