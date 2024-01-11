ca0965e32bf023b13a44285b30d8b17aead62d29
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-01-11 10:28:31 -0600

    folder moves

---
+++ #_dynamic_programming/lc0010-regex-matching/Solution.java
@@ -0,0 +1,52 @@
+/**
+ * @author: orc-dev
+ * @update: Jan.9 2024
+ * 
+ * @leetcode: 10.Regular Expression Matching
+ * @tag: dp (mealy)
+ * 
+ * dp.recursion
+ *   - case: char-with-'*'
+ * 
+ *     [    ][TRUE]    [    ][    ]
+ *     [    ](    ) or [TRUE](p.m.)
+ * 
+ *   - case: single-char
+ * 
+ *     [TRUE][    ]
+ *     [    ](p.m.)
+ * 
+ * Performance
+ *   - Runtime: (1 ms)
+ *   - Memory: (skip)
+ */
+class Solution {
+    public boolean isMatch(String str, String ptn) {
+        final char[] p = ptn.toCharArray();
+        final char[] s = str.toCharArray();
+        final int m = s.length;
+
+        final boolean[] dp = new boolean[m + 1];
+        dp[0] = true;  // set init-empty-string match
+
+        for (int i = 0; i < p.length; ++i) {
+            // Case: char + asterisk
+            if (i + 1 < p.length && p[i + 1] == '*') {
+                // forward traversal
+                for (int j = 1; j <= m; ++j) {
+                    dp[j] |= dp[j - 1] && (p[i] == s[j - 1] || p[i] == '.');
+                }
+                i++;  // skip the following '*'
+            }
+            // Case: single char
+            else {
+                // backward traversal
+                for (int j = m; j >= 1; --j) {
+                    dp[j] = dp[j - 1] && (p[i] == s[j - 1] || p[i] == '.');
+                }
+                dp[0] = false;  // clear init-empty-string match
+            }
+        }
+        return dp[m];
+    }
+}
---
+++ #_dynamic_programming/lc0010-regex-matching/note.md
@@ -0,0 +1,42 @@
+### Leetcode 10. Regular Expression Matching
+
+#### @ DP State Specification
+ - On the i-th iteration, `dp[j]` represents the matching state between `pattern[0:i]` and `string[0:j - 1]`.
+
+#### @ DP Tabular Representation
+    ```java
+           j: 0     1     2     3     4     5     6    7      8     9 
+            | "" | "c" | "a" | "t" | "a" | "n" | "d" | "d" | "o" | "g" | (string: "catanddog")
+     i | "" |TRUE|  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  | (init dp table)   
+     0 |"c" |
+     1 |"a*"|
+     2 |"g*"|
+     3 |".*"|
+     4 |"d*"|
+     5 |"d" |
+     6 |"o" |
+     7 |"g" |  - |  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  | ans |
+    
+       (patten: "ca*g*.*d*dog")
+    ```
+
+#### @ DP Recursion
+ - 'char with asterisk'
+    - If the top cell is true, current cell is true. Since current pattern token can match to empty string.
+    - If the left cell is true and current cell has point-match, then current cell is true.
+    ```java
+        | TRUE |          |      |      |
+        |_curr_|    or    | TRUE | _pm_ |
+    ```
+
+ - 'single char'
+    - If top-left cell is true and current cell has point-match, then current cell is true.
+    ```java
+        | TRUE |      | 
+        |      | _pm_ |
+    ```
+
+#### @ DP Implementation
+ - For 'char with asterisk' case, we do forward traversal.
+ - For 'single char' case, we do backward tranversal.
+ 
