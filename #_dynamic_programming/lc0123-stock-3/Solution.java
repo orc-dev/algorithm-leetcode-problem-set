7a5cd700a711f3ac7cf955f094895bfb989e9b18
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-01-13 18:04:39 -0600

    add LC.123 solution

---
+++ #_dynamic_programming/lc0123-stock-3/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * @author: orc-dev
+ * @update: Jan.13 2024
+ * 
+ * @leetcode: Best Time to Buy and Sell Stock III
+ * @tag: dp (mealy)
+ * 
+ * dp.state.diagram
+ *             -price           +price            -price           +price
+ *    { INIT } ------> { Buy1 } ------> { Sell1 } ------> { Buy2 } ------> { Sell2 }
+ *      |__^             |__^             |__^              |__^             |__^
+ * 
+ * dp.specification
+ *   "On the i-th day, dp.state represents the maximum profit can be achieved at that state"
+ * 
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int b1 = Integer.MIN_VALUE;
+        int s1 = 0;
+        int b2 = Integer.MIN_VALUE;
+        int s2 = 0;  // max profit with at most 2 transactions
+
+        for (int p : prices) {
+            s2 = Math.max(s2, s1 + p);
+            b2 = Math.max(b2, s1 - p);
+            s1 = Math.max(s1, b1 + p);
+            b1 = Math.max(b1, -p);
+        }
+        return s2;
+    }
+}
