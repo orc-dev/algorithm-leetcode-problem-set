9ff4f85223006e2badfc5a738147a6ba427aa8f3
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-06-01 15:12:54 -0500

    add updated solution_2

---
+++ lc0335-self-crossing/solution_2/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * @author: orc-dev
+ * @update: Jun.01 2024
+ * 
+ * @leetcode: 335. Self Crossing
+ * @tag: math, geometry
+ * 
+ * @update:
+ *   - Convert `hitSelf()` to `comp()`
+ * 
+ * Performance
+ *   - Runtime: (1 ms), we only do once 'outer-wall-htting' check
+ *   - Memory: constant
+ */
+class Solution {
+    public boolean isSelfCrossing(int[] dist) {
+        int p = 2;
+        // increasing stage
+        while (p < dist.length && dist[p] > dist[p - 2]) {
+            p++;
+        }
+        p++;
+        // check point
+        if (comp(dist, p) && comp(dist, p - 1)) {
+            return true;
+        }
+        // decreasing stage
+        while (p < dist.length && dist[p] < dist[p - 2]) {
+            p++;
+        }
+        return p < dist.length;
+    }
+
+    /** Compare [p] and [p - 2] - [p - 4] */
+    private boolean comp(int[] dist, int p) {
+        if (p >= dist.length) {
+            return false;
+        }
+        final int p4 = (p >= 4) ? dist[p - 4] : 0;
+        return dist[p] >= dist[p - 2] - p4;
+    }
+}
