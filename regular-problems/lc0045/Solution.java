093cf7bd8582a10d20c23fa4f9d5275595404f9a
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-06-05 09:14:32 -0500

    update 0045

--- regular-problems/lc0045/Solution.java
+++ regular-problems/lc0045/Solution.java
@@ -1,13 +1,13 @@
 /**
  * @author: orc-dev
- * @update: Jan.10 2024
+ * @update: Jan.10 2024 | Jun.05 2024
  * 
  * @leetcode: 45.Jump Game II
  * @tag: bfs
  * 
  * Performance:
- *   - Runtime: (..)
- *   - Memory: (..)
+ *   - Runtime: (0 ms)
+ *   - Memory: constant
  */
 class Solution {
     public int jump(int[] nums) {
@@ -15,28 +15,31 @@ class Solution {
         if (nums.length == 1) {
             return 0;
         }
-        return bfs(nums, 0, 0, 0, 0);
+        return bfs(nums, 0, 0, 0);
     }
 
     /**
      * An recursive implementation of BFS algorithm, where we only use 'nextEnd'
      * as a checking parameter for termination.
      * 
-     * @param nums - data
-     * @param i - index of current visiting element
-     * @param currEnd - ending index of current level
-     * @param nextEnd - ending index of next level
-     * @param step - number of jumps
+     * @param nums      - data
+     * @param currStart - starting index of current level
+     * @param currEnd   - ending index of current level
+     * @param step      - jump count
      * @return the minimum number of jumps to reach the end
      */
-    private int bfs(int[] nums, int i, int currEnd, int nextEnd, int step) {
-        nextEnd = Math.max(nextEnd, i + nums[i]);
-        if (nextEnd >= nums.length - 1)
-            return step + 1;
-        
-        if (i < currEnd)
-            return bfs(nums, i + 1, currEnd, nextEnd, step);
-        else
-            return bfs(nums, i + 1, nextEnd, nextEnd, step + 1);
+    private int bfs(int[] nums, int currStart, int currEnd, int step) {
+        int nextEnd = currEnd;
+        for (int i = currStart; i <= currEnd; ++i) {
+            nextEnd = Math.max(nextEnd, i + nums[i]);
+            if (nextEnd >= nums.length - 1) {
+                return step + 1;
+            }
+        }
+        // Optional: unable to reach the end
+        if (nextEnd == currEnd) {
+            return -1;
+        }
+        return bfs(nums, currEnd + 1, nextEnd, step + 1);
     }
 }
