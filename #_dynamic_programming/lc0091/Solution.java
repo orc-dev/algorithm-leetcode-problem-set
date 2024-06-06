4b22a442a4ac7f0e9717f38b6885cdd3b1532bbf
Xin Cai <xincai@Xins-MacBook-Pro.local>
2024-06-06 12:45:19 -0500

    update 0091

--- #_dynamic_programming/lc0091/Solution.java
+++ #_dynamic_programming/lc0091/Solution.java
@@ -1,6 +1,6 @@
 /**
  * @author: orc-dev
- * @update: Jan.11 2024
+ * @update: Jan.11 2024 | Jun.06 2024
  * 
  * @leetcode: 91.Decode Ways
  * @tag: dp (mealy, linear-tail)
@@ -11,41 +11,38 @@
  * 
  * Performance
  *   - Runtime: (0 ms)
- *   - Memory: (skip)
+ *   - Memory: O(1)
  */
 class Solution {
     public int numDecodings(String s) {
-        return dp(s.toCharArray(), 0, '0', 0, 1);
+        return dp(s.toCharArray(), 0, 0, 0, 1);
     }
 
     /**
-     * Forward recursive dp function to ease the value-copy-chain operations.
+     * Recursive version of dp.
      * 
-     * @param seq char array of encoded message
-     * @param i current index
-     * @param p previous char
-     * @param s2 value in state 2 steps back
-     * @param s1 value in state 1 step back
-     * @return the number of ways to decode the given string.
+     * @param seq encoded string
+     * @param i index of current char
+     * @param prev previous digit
+     * @param p2 state[i - 2]
+     * @param p1 state[i - 1]
+     * @return the number of ways to decode the sequence[0:i]
      */
-    private int dp(char[] seq, int i, char p, int s2, int s1) {
-        if (s1 == 0 || i == seq.length) {
-            return s1;
+    private int dp(char[] seq, int i, int prev, int p2, int p1) {
+        if (i == seq.length || p1 == 0) {
+            return p1;
         }
-        final char c = seq[i];
-        // dp.recursion
-        final int s0 = valid(p, c) * s2 + valid(c) * s1;
-        return dp(seq, i + 1, c, s1, s0);
-    }
-
-    /** Returns 1 if current char is decodable, otherwise 0. */
-    private int valid(char c) {
-        return (c == '0') ? 0 : 1;
-    }
-
-    /** Returns 1 if seq[i - 1: i] is decodable, otherwise 0. */
-    private int valid(char p, char c) {
-        final int num = (p - '0') * 10 + (c - '0');
-        return (num >= 10 && num <= 26) ? 1 : 0;
+        int p0 = 0;
+        // check the single-digit number
+        final int curr = seq[i] - '0';
+        if (curr >= 1 && curr <= 9) {
+            p0 += p1;
+        }
+        // check the two-digit-number
+        final int tens = prev * 10 + curr;
+        if (tens >= 10 && tens <= 26) {
+            p0 += p2;
+        }
+        return dp(seq, i + 1, curr, p1, p0);
     }
 }
