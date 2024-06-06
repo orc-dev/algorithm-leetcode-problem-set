352eea2befaab7f153eb8fc4db27c8c60f934db6
Xin Cai <xincai@Xins-MBP.attlocal.net>
2024-06-05 22:08:37 -0500

    update 0087

--- #_dynamic_programming/lc0087-scramble-string/Solution.java
+++ #_dynamic_programming/lc0087-scramble-string/Solution.java
@@ -1,98 +1,76 @@
+import java.util.Arrays;
 /**
  * @author: orc-dev
- * @update: Jan.11 2024
+ * @update: Jan.11 2024 | Jun.05 2024
  * 
  * @leetcode: 87.Scramble String
  * @tag: dp (moore, high-di)
  * 
- * implementation
- *   - Space saving
- *   - OOP: DPTable
- *   - index mapping
- * 
  * Performance
- *   - Runtime: (..)
- *   - Memory: (..)
+ *   - Runtime: O(n^4), (2 ms)
+ *   - Memory: O(n^3), (1/3)
  */
 class Solution {
-    private char[] S, T;
-    private DPTable tb;
+
+    private char[] s, t;
+    private Boolean[][][] dp;
+    private final int[] table = new int[26];
 
     public boolean isScramble(String s1, String s2) {
-        S = s1.toCharArray();
-        T = s2.toCharArray();
-        tb = new DPTable(S.length);
-        return dp(S.length, 0, 0);
+        s = s1.toCharArray();
+        t = s2.toCharArray();
+
+        final int n = s.length;
+        dp = new Boolean[n + 1][][];
+        for (int i = 1, j = n; i <= n; ++i, --j) {
+            dp[i] = new Boolean[j][j];
+        }
+        return dp(n, 0, 0);
     }
 
     /**
-     * This DP uses three dimensions to specify a subproblem state:
-     * <substring_length, starting_index_s1, starting_index_s2>.
-     * 
-     * @param length length of the substring
-     * @param s starting index of String s1
-     * @param t starting index of String s2
-     * @return true if s1.substring(s, length) and s2.substring(t, length) 
-     *         are scramble; false otherwise.
+     * @param size the size of substrings
+     * @param i starting index on s1
+     * @param j starting index on s2
+     * @return if the two substrings are scrambled
      */
-    private boolean dp(int length, int s, int t) {
-        // Base case: compare one char with aonther
-        if (length == 1) {
-            return S[s] == T[t];
+    private boolean dp(int size, int i, int j) {
+        if (dp[size][i][j] != null) {
+            return dp[size][i][j];
         }
-        // Check if result has been computed
-        final byte cachedValue = tb.get(length, s, t);
-        if (cachedValue >= 0) {
-            return cachedValue == 1;
-        }
-        // Compute new instance and store result to table
-        boolean found = false;
-        for (int a = 1; a < length && !found; ++a) {
-            final int b = length - a;
-            // dp.recursion
-            found = dp(a, s, t) && dp(b, s + a, t + a) ||
-                    dp(a, s, t + b) && dp(b, s + a, t);
-        }
-        return tb.set(length, s, t, found);
-    }
-}
 
-/**
- * Lightweight class wraps a byte[][] table for storing and fetching
- * the results of dp calculation.
- *   1: true
- *   0: false
- *  -1: default (state uncompted)
- */
-class DPTable {
-    private final byte[][] tb;
-    private final int SIZE;
-
-    /**
-     * Construct a dp table, filling with default value -1.
-     * @param size row numbers of the dp table
-     */
-    public DPTable(int size) {
-        this.SIZE = size;
-        this.tb = new byte[size][];
-
-        // Allocate array for each row with only necessary capacity
-        for (int i = size - 1, k = 1; i >= 0; --i, ++k) {
-            tb[i] = new byte[k * k];
-            java.util.Arrays.fill(tb[i], (byte)(-1));
+        if (!sameFreq(size, i, j)) {
+            return dp[size][i][j] = false;
         }
+
+        if (size == 1) {
+            return dp[size][i][j] = (s[i] == t[j]);
+        }
+
+        for (int k = 1; k < size; ++k) {
+            final int q = size - k;
+
+            if (dp(k, i, j    ) && dp(q, i + k, j + k) || 
+                dp(k, i, j + q) && dp(q, i + k, j    )) {
+                return dp[size][i][j] = true;
+            }
+        }
+        return dp[size][i][j] = false;
     }
 
-    /** Set state <legnth, s, t> with given value 'val' and returns it */
-    public boolean set(int length, int s, int t, boolean val) {
-        final int index = s * (this.SIZE - length + 1) + t;
-        this.tb[length - 1][index] = (byte) (val ? 1 : 0);
-        return val;
-    }
+    /** check if two substrings contains chars with the same frequency */
+    private boolean sameFreq(int size, int i, int j) {
+        Arrays.fill(table, 0);
 
-    /** Returns value of state <legnth, s, t> */
-    public byte get(int length, int s, int t) {
-        final int index = s * (this.SIZE - length + 1) + t;
-        return this.tb[length - 1][index];
+        for (int k = i; k < i + size; ++k) {
+            table[s[k] - 'a']++;
+        }
+
+        for (int k = j; k < j + size; ++k) {
+            table[t[k] - 'a']--;
+            if (table[t[k] - 'a'] < 0)
+                return false;
+        }
+        return true;
     }
 }
